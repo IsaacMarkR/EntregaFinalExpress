ac27cdc4b5d09ace05f6c298bd42d1b894ccc4e4
IsaacMark <veronicamuete@Marlons-MacBook-Pro.local>
2024-04-29 22:18:34 -0500

    Adding Cart full functionalities and persitent data for cart

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const port = 8020;
 app.use(express.json());
 
 const productRoutes = require('./productRoutes');
+const cartRoutes = require('./cartRoutes');
 app.use('/products', productRoutes);
+app.use('/api/carts', cartRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
