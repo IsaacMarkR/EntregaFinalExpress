f0e0f7cb44f23ba26a520bcfd51316e6d1b34e4a
IsaacMark <veronicamuete@Marlons-MacBook-Pro.local>
2024-04-29 21:51:45 -0500

    Adding POST method y logic behind

--- productRoutes.js
+++ productRoutes.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs').promises;
+const { v4: uuidv4 } = require('uuid');
 const path = './products.json';
+
+async function loadProducts() {
+    try {
+        const data = await fs.readFile(path, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        throw new Error('Error reading the products file');
+    }
+}
 
 router.get('/', async (req, res) => {
     try {
@@ -12,10 +22,40 @@ router.get('/', async (req, res) => {
         if (limit) {
             products = products.slice(0, limit);
         }
-
         res.json(products);
     } catch (error) {
         res.status(500).json({ error: 'Error reading the products file' });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const { title, description, code, price, stock, category, thumbnails = [] } = req.body;
+        if (!title || !description || !code || !price || !stock || !category) {
+            return res.status(400).send('Missing required fields');
+        }
+        const products = await loadProducts();
+        if (products.some(product => product.code === code)) {
+            return res.status(409).send('Product with this code already exists');
+        }
+
+        const newProduct = {
+            id: uuidv4(),
+            title,
+            description,
+            code,
+            price,
+            status: true, 
+            stock,
+            category,
+            thumbnails
+        };
+        products.push(newProduct);
+
+        await fs.writeFile(path, JSON.stringify(products, null, 2), 'utf8');
+        res.status(201).json(newProduct);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to add product' });
     }
 });
 
